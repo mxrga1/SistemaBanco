ac9bd143a35ce2f309944d04025557e11b99e553
Vinicius Margarida <vinicius_margarida@estudante.sesisenai.org.br>
2023-02-16 16:05:44 -0300

    criar classe commit

---
+++ Cliente.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaBanco
+{
+    public class Cliente
+    {
+        //public List<Transacao> Extrato { get; set; }
+        public double Saldo { get; set; }
+        public int NumeroConta { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using SistemaBanco;
+
+
+Cliente CriarConta()
+{
+
+    Cliente cliente = new Cliente();
+
+    Console.WriteLine("Nome do cliente:");
+    cliente.Nome = Console.ReadLine();
+
+    Console.WriteLine("CPF do cliente:");
+    cliente.CPF = Console.ReadLine();
+
+    Console.WriteLine("Endereço do cliente:");
+    cliente.Endereco = Console.ReadLine();
+
+    Console.WriteLine("Data de Nascimento do cliente:");
+    cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+    Console.WriteLine("Telefone do cliente:");
+    cliente.Telefone = Console.ReadLine();
+
+    Console.WriteLine("Email do cliente:");
+    cliente.Email = Console.ReadLine();
+
+    Console.WriteLine("Numero da conta do cliente:");
+    cliente.NumeroConta = int.Parse(Console.ReadLine());
+
+    return cliente;
+
+}
+
+
+
+
+
+
+string opcao;
+do
+{
+    Console.WriteLine("Bem vindos ao Banco FULL STACK BANK, escolha uma opção");
+    Console.WriteLine("1 - Criar Conta ");
+    Console.WriteLine("2 - Adicionar Transação");
+    Console.WriteLine("3 - Consultar Extrato");
+    Console.WriteLine("4 - Sair!");
+    opcao = Console.ReadLine();
+
+    if(opcao == "1"){
+      Cliente novoCliente = CriarConta();
+    }
+} while (opcao != "3");
